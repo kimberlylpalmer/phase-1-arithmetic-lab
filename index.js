22f0f104688653d5e6834f37c68407f4a6495c88
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-10-29 18:09:19 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random()+1);
+
+const num3 = 5
+const num4 = 19
+const mod = num4 % num3
+
+
+const max = Math.max(1, 2, 3, 5, 20)
